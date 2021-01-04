c6498b42c5cf8ff7d66a1f01526680d0d118bafd
FPVogel <52160477+FPVogel@users.noreply.github.com>
2021-01-04 14:46:48 +0100

    Update __init__.py

--- chatchart/__init__.py
+++ chatchart/__init__.py
@@ -1,7 +1,8 @@
-from .chatchart import Chatchart
-
-__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
+from .schreibzeig import Schreibzeig
 
 
+    __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
+
+    
 def setup(bot):
-    bot.add_cog(Chatchart(bot))
+    bot.add_cog(Schreibzeig())
